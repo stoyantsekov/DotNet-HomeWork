8cc3b41d68f3e2d9605311944f8fdeacbfa0af91
stoyan.tsekov <stoyan.tsekov@gmail.com>
2015-07-20 21:30:57 +0300

    Data Types

---
+++ Data Types/OurFirstProgram/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("What is your name: ?");
+            //string name = Console.ReadLine();
+            //Console.Write("Your name is: ");
+            //Console.WriteLine(name);
+
+
+            //Console.WriteLine("Please enter a number");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine(x);
+
+            //int test;
+            //bool succes = int.TryParse(Console.ReadLine(), out test);
+            //Console.WriteLine(succes);
+
+            //int a = 22;
+            //int b = 7;
+
+            //Console.WriteLine(a % (b-1));
+            //Console.WriteLine(Math.Sqrt(25));
+
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+
+            //string fisrt = "bod" ;
+            //Console.WriteLine(fisrt[2]);
+
+            //byte testByte = 255;
+            //testByte += 8;
+            //Console.WriteLine(testByte);
+
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine(a||b);
+            
+
+        }
+
+    }
+
+}
